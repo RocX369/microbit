928242b385a9a108753a8059f874fc638fa455c2
RocX369 <84703372+RocX369@users.noreply.github.com>
2023-08-24 10:47:05 +0200

    Add files via upload

---
+++ ldr_SpeedCalc/ldr_speed.py
@@ -0,0 +1,24 @@
+from microbit import *
+
+ldr1 = pino
+ldr2 = pin1
+led1 = pin8
+led2 = pin12
+
+distance = 40			# ldr sensors are 40cm appart
+count = 0
+start = 1
+
+while True:
+    if ldr1.read_analog() > 300:
+        start = 1
+    while sart == 1:
+        sleep(10)
+        count = count + 1
+        if ldr2.read_analog() > 300:
+            t = count/100
+            start = 0
+            break
+
+speed = distance/t
+print(speed)
