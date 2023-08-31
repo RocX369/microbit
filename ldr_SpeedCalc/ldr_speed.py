968abf7c3d54aef051e18475ac16f2f47579272c
LigmaZa <wihankepler22b@gmail.com>
2023-08-31 14:09:55 +0200

    wihan

--- ldr_SpeedCalc/ldr_speed.py
+++ ldr_SpeedCalc/ldr_speed.py
@@ -1,6 +1,6 @@
 from microbit import *
 
-ldr1 = pino
+ldr1 = pin0
 ldr2 = pin1
 led1 = pin8
 led2 = pin12
@@ -10,16 +10,18 @@ count = 0
 start = 1
 
 while True:
-    if ldr1.read_analog() > 300:
+    val_1 = ldr1.read_analog()
+    if ldr1.read_analog() > 400:
         start = 1
-    while sart == 1:
+    while start == 1:
         sleep(10)
         count = count + 1
-        if ldr2.read_analog() > 300:
+        if ldr2.read_analog() > 400:
             t = count/100
             start = 0
             break
-
+    print(val_1)
+    
 speed = distance/t
-print(speed)
+print(val_1)
 #wihan
